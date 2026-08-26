cd394d94465214e4241a3e0e41b4735d4e631f7d
Cc <455457721@qq.com>
2026-08-26 16:38:47 +0800

    chore: 清理Vivado脚本并添加一键工程配置

--- prj/check_clean_bd_min.tcl
+++
@@ -1,8 +0,0 @@
-puts "CHECK_START"
-set script_dir [file dirname [file normalize [info script]]]
-open_project [file join $script_dir sixteen_camera.xpr]
-puts "PROJECT_OPEN"
-set bd_file [get_files -quiet */design_1.bd]
-puts "BD_COUNT=[llength $bd_file]"
-close_project
-puts "CHECK_DONE"
--- prj/check_phase1_rtl_clean.tcl
+++
@@ -1,9 +0,0 @@
-set script_dir [file dirname [file normalize [info script]]]
-open_project [file join $script_dir sixteen_camera.xpr]
-set_property AUTO_INCREMENTAL_CHECKPOINT 0 [get_runs synth_1]
-set_property INCREMENTAL_CHECKPOINT {} [get_runs synth_1]
-update_compile_order -fileset sources_1
-synth_design -rtl -name rtl_phase1_clean -top top_wrapper -part xcvu13p-fhga2104-2-i
-puts "PHASE1_RTL_ELABORATION_CLEAN=PASS"
-close_design
-close_project
